dd2696d627bedeadea50d354d9ae1b41ea13a9e3
thanhhaudev <thanhhaudev@gmail.com>
2024-08-19 15:49:28 +0700

    add render functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,78 @@
 package main
 
 import (
+	"fmt"
+	"math"
+	"strings"
+
 	"github.com/joho/godotenv"
 )
+
+const (
+	nameLength        = 25
+	descriptionLength = 20
+	graphLength       = 25
+)
+
+type Data struct {
+	Name        string
+	Description string
+	Percent     float64
+}
 
 func init() {
 	godotenv.Load()
 }
 
 func main() {
-	// todo
+
+}
+
+func makeList(d ...Data) string {
+	if len(d) == 0 {
+		return ""
+	}
+
+	var b strings.Builder
+	for _, v := range d {
+		b.WriteString(formatData(v))
+	}
+
+	return b.String()
+}
+
+func makeGraph(p float64) string {
+	d, e, q := "█", "░", math.Round(p/(100/graphLength))
+
+	return strings.Repeat(d, int(q)) + strings.Repeat(e, graphLength-int(q))
+}
+
+func formatData(v Data) string {
+	var b strings.Builder
+
+	n := truncateString(v.Name, nameLength)
+	d := truncateString(v.Description, descriptionLength)
+
+	b.WriteString("\n")
+	b.WriteString(n)
+	b.WriteString(strings.Repeat(" ", nameLength-len(n)))
+	b.WriteString(d)
+	b.WriteString(strings.Repeat(" ", descriptionLength-len(d)))
+	b.WriteString(makeGraph(v.Percent))
+	b.WriteString("   ")
+	b.WriteString(formatPercent(v.Percent))
+
+	return b.String()
+}
+
+func formatPercent(p float64) string {
+	return fmt.Sprintf("%05.2f%%", p)
+}
+
+func truncateString(s string, l int) string {
+	if len(s) > l {
+		return s[:l]
+	}
+
+	return s
 }
